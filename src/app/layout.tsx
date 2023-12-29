f493488e09b59edfc11910a093fcd3c50ec1fb58
johnsonsu <johnsonsu0603@gmail.com>
2023-12-29 16:07:35 -0500

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from 'next'
-import { IBM_Plex_Mono } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { IBM_Plex_Mono } from "next/font/google";
+import "./globals.css";
 
-const inter = IBM_Plex_Mono({ subsets: ['latin'], weight: ['400'] })
+const inter = IBM_Plex_Mono({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata: Metadata = {
-    title: 'Johnson Su 🧑🏻‍💻',
-    description:
-        "Hi there 👋, I'm a full-stack software engineer from Toronto. Nice meeting you!",
-}
+  title: "Johnson Su 🧑🏻‍💻",
+  description:
+    "Hi there 👋, I'm a full-stack software engineer from Toronto. Nice meeting you!",
+};
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <head>
-                <meta charSet="utf-8"></meta>
-            </head>
-            <body className={inter.className}>{children}</body>
-        </html>
-    )
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8"></meta>
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
